5c21f5879f19f041b5371eb600693a145d63cbe3
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2020-04-03 18:00:42 -0300

    Removidas as pastas e adicionado somente os arquivos.

---
+++ 1005.py
@@ -0,0 +1,9 @@
+# URI [1004]
+# Pegar as duas variáveis
+# Se utilizar python 2.x deve-se trocar o input() por raw_input()
+x = float(input())
+y = float(input())
+
+media = ((x * 3.5) + (y * 7.5)) / (3.5 + 7.5)
+
+print("MEDIA = {0:.5f}".format(media))
---
+++ 1006.py
@@ -0,0 +1,10 @@
+# URI [1004]
+# Pegar as duas variáveis
+# Se utilizar python 2.x deve-se trocar o input() por raw_input()
+x = int(input())
+y = int(input())
+
+print("PROD = {0}".format(x * y))
+
+# Outra forma de fazer
+# print("PROD = " + str(x * y))
